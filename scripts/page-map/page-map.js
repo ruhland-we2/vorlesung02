11adc6ca12c4fb1a49f66b62dbda1409f506b42e
ruhland-we2 <k.ruhland@hszg.de>
2022-03-24 13:21:28 +0100

    leaflet

--- scripts/page-map/page-map.js
+++ scripts/page-map/page-map.js
@@ -4,10 +4,25 @@ const page_map_markup = `
         <div class="title">Vorlesung02 Map</div>
     </div>
     <div id="page-map-area">
+        <div class="area"></div>
     </div>
 </div>
 `;
 
+const osmUrl = 
+ "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}";
+//   'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
+
+
+
 function show_page_map(){
     document.body.insertAdjacentHTML("beforeend",page_map_markup);
+
+    let osmLayer = new L.TileLayer(osmUrl);
+    let maparea = document.querySelector("#page-map-area .area");
+    let map = new L.Map(maparea);
+    setTimeout(function(){ 
+        map.setView(new L.LatLng(51.147990,14.997320), 14);
+    },200);
+    map.addLayer(osmLayer);  
 }
